913a96c090d70a138017060be2033ecc036a50bd
hanse <thehanse@gmail.com>
2013-03-20 15:33:16 +0100

    basic project

---
+++ project1/MultiSnake/src/mulstisnackeclient/MultiSnakeClient.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mulstisnackeclient;
+
+/**
+ *
+ * @author hanse
+ */
+public class MultiSnakeClient {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ich bin der Client!");
+    }
+}
