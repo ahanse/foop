74d629e51f7994049894ab8adb62859d872ffdca
Max Bk <iwaniwanowitsch1@web.de>
2013-03-21 10:27:40 +0100

    Work in progress: get,set,constr for point
    added getters, setters and contructor for class Point. initialized
    variables as private.

--- project1/MultiSnake/src/multisnakeglobal/Point.java
+++ project1/MultiSnake/src/multisnakeglobal/Point.java
@@ -9,6 +9,27 @@ package multisnakeglobal;
  * @author Benedikt
  */
 public class Point {
-    int X;
-    int Y;
+    private int X;
+    private int Y;
+
+    public Point(int X, int Y) {
+        this.X = X;
+        this.Y = Y;
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public void setX(int X) {
+        this.X = X;
+    }
+
+    public int getY() {
+        return Y;
+    }
+
+    public void setY(int Y) {
+        this.Y = Y;
+    }
 }
