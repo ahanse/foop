c0fffe134af54d23875b56c63786be46fa80b539
Max Bk <iwaniwanowitsch1@web.de>
2013-05-03 14:55:19 +0200

    standard value tweak

--- project1/MultiSnake/src/multisnakeclient/NewServerPanel.java
+++ project1/MultiSnake/src/multisnakeclient/NewServerPanel.java
@@ -39,7 +39,7 @@ public class NewServerPanel extends JPanel implements ActionListener {
         // create Options
         JLabel lblTick = new JLabel("Game tick(ms)");
         txtTick = new JTextField(10);
-        txtTick.setText("500");
+        txtTick.setText("200");
         JLabel lblDimX = new JLabel("Game Board X");
         txtDimX = new JTextField(10);
         txtDimX.setText("30");
